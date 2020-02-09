a2ea9d641609bac525562aa224f2fc4c5f77bb6c
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-02-08 16:25:35 -0800

    created models

---
+++ models/burger.js
@@ -0,0 +1,55 @@
+var orm = require('../config/orm')
+
+
+
+var burger = {
+
+all: function(cb) {
+
+    orm.selectAll("burgers", function(res) {
+
+    cb(res)
+
+    });
+
+},
+
+
+
+create: function(cols, vals, cb) {
+
+    orm.insertOne("burgers", cols, vals, function(err,res) {
+
+        cb(res)
+
+
+
+    });
+
+},
+
+updateOne: function(objColVals, condition, cb) {
+
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+
+        cb(res)
+
+    });
+
+},
+
+delete: function(condition, cb) {
+
+    orm.delete("burgers", condition, function(res) {
+
+        cb(res);
+
+    });
+
+}
+
+};
+
+
+
+module.exports = burger;
